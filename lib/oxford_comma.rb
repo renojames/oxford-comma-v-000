78845dc8915c23473c801c38d52d6bebde3b81e4
Reno Sorensen <renojames19@gmail.com>
2018-02-16 04:58:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count != 1 && array.count != 2
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
